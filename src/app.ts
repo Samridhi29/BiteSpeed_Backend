54d665b5d6d1b05f46ef693df60282b88452d29e
samridhi29 <jhasamridhi29@gmail.com>
2025-07-18 15:09:30 +0530

    Add route, controller, and service logic

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-import express from 'express';
-import identifyRoutes from './routes/identify.routes';
+import express from "express";
+import dotenv from "dotenv";
+import identifyRoute from "./routes/identify.route";
+import errorHandler from "./middlewares/error.middleware";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use('/identify', identifyRoutes);
+app.use("/identify", identifyRoute);
+
+// Error Handling Middleware
+app.use(errorHandler);
 
 export default app;
+
